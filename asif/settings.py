f392488539116491582a5f0b3aeed642291a0443
Asif-686 <asif15-10686@diu.edu.bd>
2021-06-12 21:51:31 +0600

    update settings

--- asif/settings.py
+++ asif/settings.py
@@ -125,6 +125,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
